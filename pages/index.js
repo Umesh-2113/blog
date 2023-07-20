c8173ead1226150e1ab34366ec56c594d5dc948c
Umesh-2113 <139538819+Umesh-2113@users.noreply.github.com>
2023-07-20 17:54:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import NavBar from '@/components/NavBar'
-import FeaturedCard from '@/components/FeaturedCard'
-import Feed from '../components/Feed'
-import Categories2 from '@/components/Categories2'
-import Allpost from '@/components/Allpost'
-import Posts from '@/components/Posts'
+  import Image from 'next/image'
+  import { Inter } from 'next/font/google'
+  import NavBar from '@/components/NavBar'
+  import FeaturedCard from '@/components/FeaturedCard'
+  import Feed from '../components/Feed'
+  import Categories2 from '@/components/Categories2'
+  import Allpost from '@/components/Allpost'
+  import Posts from '@/components/Posts'
 
 
-const inter = Inter({ subsets: ['latin'] })
+  const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <div className='max-w-7xl mx-auto'>
-      <NavBar/>
-      <FeaturedCard/>
-      <Allpost/>
+  export default function Home() {
+    return (
+      <div className='max-w-7xl mx-auto'>
+        <NavBar/>
+        <FeaturedCard/>
+        <Allpost/>
 
-      <Categories2/>
-      <Posts/>
-      {/* <section className='absolute md:-mt-14  lg:mt- w-full'>
-       <Feed/>
-      </section> */}
+        <Categories2/>
+        <Posts/>
+        {/* <section className='absolute md:-mt-14  lg:mt- w-full'>
+        <Feed/>
+        </section> */}
 
-
-    </div>
-  )
-}
+  <footer>
+  <h1 className='text-white text-center font-mono'> • © FullStalk - A Property of <span className='text-amber-300 font-mono'>LeadcellB2B</span>  •</h1>
+  </footer>
+      </div>
+    )
+  }
