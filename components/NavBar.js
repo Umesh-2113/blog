e724b4d584cc8b59a0b2694d13d5d3e7226f76e5
Umesh-2113 <139538819+Umesh-2113@users.noreply.github.com>
2023-07-20 17:55:18 +0530

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,40 +1,32 @@
-import React from 'react'
-import {FaInstagram, FaTwitter, FaLinkedin, FaGithub} from 'react-icons/fa';
+import React from 'react';
+import { FaInstagram, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div className=' flex bg-black pb-8 text-white'>
-    <div className=' flex'>
-            <img src= 'p.webp' alt='image' className='h-12'/>
+    <div className='flex bg-black pb-8 text-white'>
+      <div className='flex'>
+        <img src='p.webp' alt='image' className='h-12' />
+      </div>
+      <div className=''>
+        <h1 className='md:text-2xl text-xs text-white font-mono font-extrabold pt-4'>Full Stalk</h1>
+      </div>
+      <div className='w-[30%]'></div>
+      <div className='flex mx-auto space-x-8 md:ml-96 mt-4'>
+        <a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
+          <FaInstagram size={18} className='hover:text-purple-300' />
+        </a>
+        <a href='https://in.linkedin.com/' target='_blank' rel='noopener noreferrer'>
+          <FaLinkedin size={18} className='hover:text-purple-300'/>
+        </a>
+        <a href='https://twitter.com/' target='_blank' rel='noopener noreferrer'>
+          <FaTwitter size={18} className='hover:text-purple-300'/>
+        </a>
+        <a href='https://github.com/' target='_blank' rel='noopener noreferrer'>
+          <FaGithub size={18} className='hover:text-purple-300'/>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-           
-            </div>
-            <div className=' '>
-            <h1 className='md:text-2xl text-xs text-white font-mono font-extrabold pt-4'>Full Stalk</h1>
-            </div>
-<div className='w-[30%]'></div>
-            <div className=' flex mx-auto space-x-8 md:ml-96   mt-4'>
-              <a
-              href='#'>
-                <FaInstagram size={18}/>
-              </a>
-              <a
-              href='#'>
-              <FaLinkedin size={18}/>
-              </a>
-              <a href='#'>
-              <FaTwitter size={18}/>
-              </a>
-              
-              <a href='#'>
-              <FaGithub size={18}/>
-              </a>
-
-               </div>
-               
-        </div>
-    
-  )
-}
-
-export default NavBar
+export default NavBar;
